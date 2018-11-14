1090695cd76e17acaea29f841acd16ba50d762f9
Mario Mupo <mario.mupo@outlook.com>
2018-11-14 11:59:24 +0100

    es2

--- pkg_1/esercizio2.py
+++ pkg_1/esercizio2.py
@@ -1,9 +1,41 @@
 from pkg_1.esercizio1 import NewAVLTreeMap
 
 
-class Statistics:
-    def __init__(self, k, f, t):
-        self._key = k
-        self._frequency = f
-        self._total = t
+class Statistics(NewAVLTreeMap):
+    class _Element:
+        def __init__(self, f, t):
+            self._frequency = f
+            self._total = t
+
+    def __init__(self):
+        super().__init__(None)
         self._occurrency = 0
+
+    def add(self, k, v):
+        try:
+            el = self.__getitem__(k)
+            el._frequecy += 1
+            el._total += v
+            self.__setitem__(k, el)
+        except KeyError:
+            elem = self.Element(1, v)
+            self.__setitem__(k, elem)
+
+
+    def len(self):
+        pass
+
+    def occurrences(self):
+        pass
+
+    def average(self):
+        pass
+
+    def median(self):
+        pass
+
+    def percentile(self, j=20):
+        pass
+
+    def mostFrequent(self,j):
+        pass
