81dbb04364c57637ad6bda02d45ec31472f9cf6e
Mario Mupo <mario.mupo@outlook.com>
2018-11-14 09:37:53 +0100

    es2

---
+++ pkg_1/esercizio2.py
@@ -0,0 +1,9 @@
+from pkg_1.esercizio1 import NewAVLTreeMap
+
+
+class Statistics:
+    def __init__(self, k, f, t):
+        self._key = k
+        self._frequency = f
+        self._total = t
+        self._occurrency = 0
