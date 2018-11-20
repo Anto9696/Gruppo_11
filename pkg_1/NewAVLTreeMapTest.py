e0a35a856667236ea8d3737c612d4e1a89a81e00
Anto9696 <anto>
2018-11-20 15:10:53 +0100

    Test 1 ok

--- pkg_1/NewAVLTreeMapTest.py
+++ pkg_1/NewAVLTreeMapTest.py
@@ -1,19 +1,30 @@
 from pkg_1.esercizio1 import NewAVLTreeMap
 
+# Stampa l'albero tree per livelli. Ogni coppia (k b) k è chiave e b è fattore di bilanciamento
 def visual_tree(tree):
-    if(isinstance(tree,NewAVLTreeMap)):
+    if isinstance(tree,NewAVLTreeMap):
         level=""
         for node in tree.breadthfirst():
             level += "("+str(node.element()._key)+" "+str(node._node._balance_factor)+")"
         print(level)
+
 
 def delete_key(tree,k):
     print("Deleting "+str(k))
     del tree[k]
     visual_tree(tree)
 
-if __name__=="__main__":
+
+def scegli():
+    print("1. Insert a node ")
+    print("2. Delete a node ")
+    print("3. Exit")
+    scelta = int(input())
+    return scelta
+
+if __name__ == "__main__":
     tree = NewAVLTreeMap()
+
     for i in range(10):
         tree[i]=i
         print("INSERT "+str(i))
@@ -35,3 +46,16 @@ if __name__=="__main__":
     delete_key(tree,6)
     delete_key(tree,3)
     delete_key(tree,8)
+
+    i = scegli()
+    while i!=3:
+        if i == 1:
+            val = int(input("Inserisci chiave "))
+            tree[val]=val
+            visual_tree(tree)
+        elif i == 2:
+            val = int(input("Inserisci chiave "))
+            delete_key(tree,val)
+        else:
+            print("Not a good choice!")
+        i = scegli()
