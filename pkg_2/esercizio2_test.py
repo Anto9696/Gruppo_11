c9894162c0051311401528da8c26bbb7c9f073db
Romeo1996 <romeo.rinaldi1996@gmail.com>
2018-11-25 15:35:13 +0100

    ok

--- pkg_2/esercizio2_test.py
+++ pkg_2/esercizio2_test.py
@@ -5,25 +5,8 @@ if __name__ == "__main__":
     print("LEN", stat.len())
     print("MEDIA", stat.average())
     print("OCCORRENZE", stat.occurrences())
-    print("PERCENTILE", stat.percentile())
-
-    print("INSERISCO VALORI")
-    for i in range(1,10):
-        stat.add(i, i)
-
-    print("SECONDO FOR")
-    for i in range(1,3):
-        stat.add(i, i)
-
-    stat.add(2, 2)
-    print("key ", 2)
-    print("LEN", stat.len())
-    print("MEDIA", stat.average())
-    print("OCCORRENZE", stat.occurrences())
-
-    print("PERCENTILE", stat.percentile(55))
+    print("PERCENTILE", stat.percentile(10))
     print("MEDIANA", stat.median())
-
     print("PIU' FREQUENTI")
 
     mf = stat.mostFrequent(3)
